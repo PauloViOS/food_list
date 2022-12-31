042cc3d2f9adc25d622bb12a287c15cdca31e69b
Paulo Santos <paulo.santos@buser.com.br>
2022-12-31 17:20:15 -0300

    Shows all items on index page

--- food_list/food/views.py
+++ food_list/food/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
 
 
 def index(request):
-	return HttpResponse('Hello World')
+	item_list = Item.objects.all()
+	context = {
+		'item_list': item_list,
+	}
+	return render(request, 'food/index.html', context)
 
 
 def item(request):
