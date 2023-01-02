b8098da7a51b758db7cb7cf524e80f0c6506f616
Paulo Santos <paulo.santos@buser.com.br>
2023-01-02 11:38:07 -0300

    Created views for deleting and editing an item

--- food_list/food/views.py
+++ food_list/food/views.py
@@ -27,7 +27,28 @@ def detail(request, item_id):
 
 def add_item(request):
 	form = ItemForm(request.POST or None)
+
 	if form.is_valid():
 		form.save()
 		return redirect('food:index')
 	return render(request, 'food/item-form.html', {'form': form})
+
+
+def edit_item(request, item_id):
+	item = Item.objects.get(id=item_id)
+	form = ItemForm(request.POST or None, instance=item)
+
+	if form.is_valid():
+		form.save()
+		return redirect('food:index')
+	return render(request, 'food/item-form.html', {'form': form, 'item': item})
+
+
+def delete_item(request, item_id):
+	item = Item.objects.get(id=item_id)
+
+	if request.method == "POST":
+		item.delete()
+		return redirect('food:index')
+
+	return render(request, 'food/item-delete.html', {'item': item})
