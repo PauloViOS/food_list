c0f6616afd8c02521da2055b89aeef61aa30c52b
Paulo Santos <paulo.santos@buser.com.br>
2023-01-02 11:39:47 -0300

    Created urls for editing and deleting

--- food_list/food/urls.py
+++ food_list/food/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('item/', views.item, name='item'),
 	path('<int:item_id>/', views.detail, name='detail'),
-	path('add', views.add_item, name='add_item'),
+	path('add/', views.add_item, name='add_item'),
+	path('edit/<int:item_id>', views.edit_item, name='edit_item'),
+	path('delete/<int:item_id>', views.delete_item, name='delete_item'),
 ]
