b115e903049b6fb641a93d25c098ae2678a2f195
Paulo Santos <paulo.santos@buser.com.br>
2023-01-02 10:17:59 -0300

    - Creates image field on item model - adds images to food items - Changes index's layout

--- food_list/food/models.py
+++ food_list/food/models.py
@@ -9,4 +9,9 @@ class Item(models.Model):
 	name = models.CharField(max_length=200)
 	description = models.CharField(max_length=200)
 	price = models.IntegerField()
-
+	# will use charfield to store the image so I don't have to save the actual image on my server
+	# I'm aware that on real life projects this is not I'll be doing
+	image = models.CharField(
+		max_length=500,
+		default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQipEtiHLzPkCFMzLEqzlkXcAb-R3Y92AE_cXZAO1VBiP4NhJrldG718z8k1MSoe6xU7zo&usqp=CAU"
+	)
