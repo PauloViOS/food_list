9e2669b30ff40e29ef8e4cc0af18a6950de66dcf
Paulo Santos <paulo.santos@buser.com.br>
2023-01-03 15:45:50 -0300

    - Profile page now shows user picture - Adds default profile pic - Adds static url conf

--- food_list/food_list/urls.py
+++ food_list/food_list/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
@@ -11,3 +13,5 @@ urlpatterns = [
 	path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 	path('profile/', user_views.profile, name='profile'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
