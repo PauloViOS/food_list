87233846839b119799b20f1616633c997c0b5ad1
Paulo Santos <paulo.santos@buser.com.br>
2023-01-03 10:53:33 -0300

    - Creates profile view, which is decorated with login_required - Creates url and template for profile page - Adds Profile link to navbar - Changes default path to login on settings

--- food_list/food_list/urls.py
+++ food_list/food_list/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
 	path('register/', user_views.register, name='register'),
 	path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
 	path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+	path('profile/', user_views.profile, name='profile'),
 ]
