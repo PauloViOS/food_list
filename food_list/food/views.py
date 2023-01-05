06f795e3f01c25316f73b0062125f73840976b9e
Paulo Santos <paulo.santos@buser.com.br>
2023-01-05 08:56:12 -0300

    - Changes index view to a class based view

--- food_list/food/views.py
+++ food_list/food/views.py
@@ -1,16 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
+from django.views.generic.list import ListView
 
 from .forms import ItemForm
 from .models import Item
 
 
-def index(request):
-	item_list = Item.objects.all()
-	context = {
-		'item_list': item_list,
-	}
-	return render(request, 'food/index.html', context)
+class IndexClassView(ListView):
+	model = Item
+	template_name = 'food/index.html'
+	context_object_name = 'item_list'
 
 
 def item(request):
