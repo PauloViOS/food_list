ff5378fede5c9f639b5fb14543e62c667c91dc42
Paulo Santos <paulo.santos@buser.com.br>
2023-01-05 09:40:37 -0300

    - Associates food with a user through foreignkey

--- food_list/food/models.py
+++ food_list/food/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -15,3 +16,4 @@ class Item(models.Model):
 		max_length=500,
 		default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQipEtiHLzPkCFMzLEqzlkXcAb-R3Y92AE_cXZAO1VBiP4NhJrldG718z8k1MSoe6xU7zo&usqp=CAU"
 	)
+	user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
